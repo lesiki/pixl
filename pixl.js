ed964414989b1a68dfedaefce6a5ef6a4ea854a2
Sitati Kituyi <sitati.kituyi@googlemail.com>
2014-04-19 21:56:05 +0300

    Added light source support, currently hardcoded on layer select

--- pixl.js
+++ pixl.js
@@ -37,7 +37,6 @@ Pixl = function() {
 	isLayerEdge = function(x, y, layerName, direction) {
 		x = parseInt(x);
 		y = parseInt(y);
-		console.log.apply(console, arguments);
 		// Assumes specified pixel is in the layer
 		if(direction === Directions.TOP) {
 			return y === 0 || $('.pixel[data-x=' + x + '][data-y=' + (y - 1) + ']').attr('data-layername') !== layerName;
@@ -110,10 +109,87 @@ Pixl = function() {
 		setTimeout(function() { $('.topEdge,.bottomEdge,.leftEdge,.rightEdge').removeClass('topEdge bottomEdge leftEdge rightEdge'); }, 1000);
 	},
 	initialiseTools = function() {
-		$('select#selectLayer').attr('disabled', 'disabled');
+		$('select#selectLayer').attr('disabled', 'disabled').change(handleLayerSelect);
+	},
+	handleLayerSelect = function() {
+		var layerName = $(this).val();
+		if(layerName !== 'noselect') {
+			flashLayerBounds(layerName);
+			addLighting(0, 0, 0.5, 0.1, layerName);
+		}
+		else {
+
+		}
+	},
+	shadeColor = function(color, percent) {
+		// from http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+		var R = parseInt(color.substring(1,3),16);
+		var G = parseInt(color.substring(3,5),16);
+		var B = parseInt(color.substring(5,7),16);
+
+		R = parseInt(R * (100 + percent) / 100);
+		G = parseInt(G * (100 + percent) / 100);
+		B = parseInt(B * (100 + percent) / 100);
+
+		R = (R<255)?R:255;  
+		G = (G<255)?G:255;  
+		B = (B<255)?B:255;  
+
+		var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+		var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+		var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+		return "#"+RR+GG+BB;
+	}
+	addLighting = function(lightSourceX, lightSourceY, maxLightIntensity, minLightIntensity, layerName) {
+		var thisPixelX, thisPixelY, distanceFromLight, baseColour, distanceAsFractionOfMaxDistance, newColour;
+		lightSourceX = parseInt(lightSourceX);
+		lightSourceY = parseInt(lightSourceY);
+		$('.pixel[data-layername=' + layerName + ']').each(function() {
+			thisPixelX = parseInt($(this).attr('data-x'));
+			thisPixelY = parseInt($(this).attr('data-y'));
+			baseColour = $(this).attr('data-baseColour');
+			distanceFromLight = Math.sqrt(((thisPixelX - lightSourceX) * (thisPixelX - lightSourceX))+ ((thisPixelY - lightSourceY) * (thisPixelY - lightSourceY)));
+			distanceAsFractionOfMaxDistance = (distanceFromLight * distanceFromLight) / (canvasSize * canvasSize);
+			newColour = shadeColor(baseColour, (minLightIntensity + ((1 - distanceAsFractionOfMaxDistance) * maxLightIntensity) * 100));
+			// Process Edges
+			if(thisPixelY > lightSourceY) {
+				if(isLayerEdge(thisPixelX, thisPixelY, layerName, Directions.TOP)) {
+					newColour = shadeColor(newColour, 5);
+				}
+				else if(isLayerEdge(thisPixelX, thisPixelY, layerName, Directions.BOTTOM)) {
+					newColour = shadeColor(newColour, -5);
+				}
+			}
+			if(thisPixelY < lightSourceY) {
+				if(isLayerEdge(thisPixelX, thisPixelY, layerName, Directions.BOTTOM)) {
+					newColour = shadeColor(newColour, 5);
+				}
+				else if(isLayerEdge(thisPixelX, thisPixelY, layerName, Directions.TOP)) {
+					newColour = shadeColor(newColour, -5);
+				}
+			}
+			if(thisPixelX > lightSourceX) {
+				if(isLayerEdge(thisPixelX, thisPixelY, layerName, Directions.LEFT)) {
+					newColour = shadeColor(newColour, 5);
+				}
+				else if(isLayerEdge(thisPixelX, thisPixelY, layerName, Directions.RIGHT)) {
+					newColour = shadeColor(newColour, -5);
+				}
+			}
+			if(thisPixelX < lightSourceX) {
+				if(isLayerEdge(thisPixelX, thisPixelY, layerName, Directions.RIGHT)) {
+					newColour = shadeColor(newColour, 5);
+				}
+				else if(isLayerEdge(thisPixelX, thisPixelY, layerName, Directions.LEFT)) {
+					newColour = shadeColor(newColour, -5);
+				}
+			}
+			$(this).css('background-color', newColour);
+		});
 	},
 	init = function() {
-		canvasSize = 10;
+		canvasSize = 60;
 		createPixels(canvasSize);
 		sizePixels(canvasSize);
 		currentColour = "#789491";
