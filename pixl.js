d1212eb67be748f50715c87d2eaeae9e83528299
Sitati Kituyi <sitati.kituyi@googlemail.com>
2014-04-19 20:33:11 +0300

    Added layer recording

--- pixl.js
+++ pixl.js
@@ -6,7 +6,9 @@ Pixl = function() {
 	var
 	canvasDimensions = 600,
 	currentColour,
-	mouseIsDown = false;
+	layerRecording = false,
+	mouseIsDown = false,
+	defaultLayerName = 'newLayer',
 	createPixels = function(countPerAxis) {
 		var canvas = $('#canvas'),
 		i, pixel;
@@ -20,29 +22,56 @@ Pixl = function() {
 		$('.pixel').addClass("s" + size);
 	},
 	handlePixelClick = function() {
-		$(this).css('background-color', currentColour).removeClass('empty');
+		$(this).css('background-color', currentColour).attr('data-baseColour', currentColour).removeClass('empty');
+		if(layerRecording) {
+			$(this).attr('data-layername', defaultLayerName);
+		}
 	},
 	bindListeners = function() {
 		$('.pixel').mousedown(handlePixelClick).mouseenter(handleMouseEntry);
 		$('#canvas').mousedown(function() { mouseIsDown = true; });
 		$('#canvas').mouseup(function() { mouseIsDown = false; });
 		$('#canvas').mouseleave(function() { mouseIsDown = false; });
+		$('#recordNewLayer').click(toggleRecording);
+	},
+	toggleRecording = function() {
+		if(layerRecording) {
+			var layerName = prompt("Please name the new layer", "LayerX");
+			$(this).html('start recording a new layer');
+			$('.pixel[data-layername=' + defaultLayerName + ']').attr('data-layername', layerName);
+			$('#selectLayer').append('<option value="' + layerName + '">' + layerName + '</option>').removeAttr('disabled');
+		}
+		else {
+			$(this).html('stop recording');
+		}
+		layerRecording = !layerRecording;
 	},
 	handleMouseDownStateChange = function(state) {
 		mouseIsDown = state;
 	},
-	handleMouseEntry = function() {
-		console.log('entered with mouse down as ' + mouseIsDown);
+	handleMouseEntry = function(e) {
 		if(mouseIsDown) {
 			// treat as a drag
 			$(this).mousedown();
+			pauseEvent(e || window.event);
 		}
+	},
+	pauseEvent = function (e){
+		if(e.stopPropagation) e.stopPropagation();
+		if(e.preventDefault) e.preventDefault();
+		e.cancelBubble=true;
+		e.returnValue=false;
+		return false;
+	},
+	initialiseTools = function() {
+		$('select#selectLayer').attr('disabled', 'disabled');
 	},
 	init = function() {
 		createPixels(10);
 		sizePixels(10);
 		currentColour = "#789491";
 		bindListeners();
+		initialiseTools();
 	};
 	init();
 };
